5218074bfff7cda4abf93e9b42447530ee7badf3
Stan Bad <stankv@mail.ru>
2021-04-22 11:57:43 +0300

    add counter for exit from circle

--- 15_TankRush.py
+++ 15_TankRush.py
@@ -27,14 +27,19 @@ def TankRush(H1, W1, S1, H2, W2, S2):
             if Matrix1[i][j] == Matrix2[0][0]:    # если найден первый символ, то проверяем может ли вмещаться в этом месте подматрица в матрицу
                 if (i + H2) <= H1 and (j + W2) <= W1:
                     # проверка вхождения подматрицы
+                    k = 0
                     for i1 in range(H2):
                         for j1 in range(W2):
                             if Matrix1[i + i1][j + j1] == Matrix2[i1][j1]:
                                 flag = True
+                                k += 1    # счетчик совпадений элементов матриц
                             else:
                                 flag = False
                                 break
+                            if k == (H2 * W2):
+                                return flag
                         if not flag:
+                            flag = False
                             break
                 else:
                     continue
