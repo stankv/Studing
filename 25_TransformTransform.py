ef51165f8071ea8d61f6b8bc6b5dd3c9b5ac07f4
Stan Bad <stankv@mail.ru>
2021-05-05 15:13:38 +0300

    fixed bug - error in index

--- 25_TransformTransform.py
+++ 25_TransformTransform.py
@@ -4,10 +4,12 @@ def TransformTransform(A, N):
     # трансформирующая трансформация
     def S(A):
         B = []
-        for i in range(len(A) - 1):
-            for j in range(len(A) - i - 1):
+        for i in range(len(A)):
+            for j in range(len(A) - i):
                 k = i + j
-                B.append(max(A[j:k], default=0))
+                ma = max(A[j:k+1], default=-1)
+                if ma > -1:    # чтобы не писать нули в массив
+                    B.append(ma)                    
         return B
 
     if sum(S(S(A))) % 2 == 0:
