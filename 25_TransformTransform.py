7c51e70186f5027a3cd6274c9d058e62a433f0f1
Stan Bad <stankv@mail.ru>
2021-05-05 10:26:53 +0300

    add 25_TransformTransform.py

---
+++ 25_TransformTransform.py
@@ -0,0 +1,16 @@
+# ПОИСК КЛЮЧЕВОГО КЛЮЧА ЧЕРЕЗ ТРАНСФОРМИРУЮЩУЮ ТРАНСФОРМАЦИЮ
+def TransformTransform(A, N):
+
+    # трансформирующая трансформация
+    def S(A):
+        B = []
+        for i in range(len(A) - 1):
+            for j in range(len(A) - i - 1):
+                k = i + j
+                B.append(max(A[j:k]))
+        return B
+
+    if sum(S(S(A))) % 2 == 0:
+        return True
+    else:
+        return False
