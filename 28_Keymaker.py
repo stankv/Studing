93e647d66c851a8985f19d3772054b4b9c74bb25
Stan Bad <stankv@mail.ru>
2021-05-10 17:03:44 +0300

    add 28_Keymaker.py

---
+++ 28_Keymaker.py
@@ -0,0 +1,22 @@
+def Keymaker(k):
+    S = []
+    stroka = ""
+    # создаем массив закрытых дверей
+    for i in range(k):
+        S.append("0")
+
+    stroka = "".join(S)
+    #print(stroka)
+    
+    n = 0
+    for i in range(k):
+        for j in range(n, k, n + 1):
+            if S[j] == "0":
+                S[j] = "1"
+            else:
+                S[j] = "0"
+        print(n + 1, " ", end = '')
+        stroka = "".join(S)
+        #print(stroka)
+        n += 1
+    return stroka
