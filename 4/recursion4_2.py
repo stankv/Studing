85beec0484a06225d02cd8a907ff10dbc3bd8d67
Stan Bad <stankv@mail.ru>
2021-05-29 20:06:53 +0300

    add recursion4_2.py

---
+++ 4/recursion4_2.py
@@ -0,0 +1,18 @@
+# Проверка, является ли строка палиндромом (используем стек)
+def CheckPalindrom(stroka):
+    stack = list(stroka.replace(' ', ''))    # удаляем пробелы из строки и заносим ее в стек
+    N = len(stroka1) // 2
+    result = True
+    for i in range(N):
+        if stack[i] == stack.pop():
+            continue
+        else:
+            result = False
+            return result
+    return result
+
+str1 = "а роза упала на лапу азора"
+if CheckPalindrom(str1):
+    print("Строка является палиндромом")
+else:
+    print("Строка НЕ является палиндромом")
