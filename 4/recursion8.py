4bc2105dad386deb2b11aeff4ca70313b803942d
Stan Bad <stankv@mail.ru>
2021-06-02 15:24:14 +0300

    modified algorithm

--- 4/recursion8.py
+++ 4/recursion8.py
@@ -7,19 +7,33 @@ def FindFiles(path):
 
     def Solving(pathtodir):
         for root, dirs, files in os.walk(pathtodir):
-            for file in files:
-                filelist.append(os.path.join(root,file))
-        return filelist
+                for file in files:
+                    filelist.append(os.path.join(root,file))
+                if dirs == []:
+                    return filelist
+                else:
+                    for dir in dirs:
+                        return Solving(pathtodir + "\\" + dir)
+
 
     filelist = []
     if not os.path.isdir(path):    #  проверка существования каталога
         return filelist
     else:
-        filelist = Solving(path)
-        return filelist
+        new_path = path
+        Solving(path)
+        for root, dirs, files in os.walk(path):
+            k = 0
+            for dirname in dirs:
+                if k == 0:
+                    k = 1
+                    continue
+                new_path = path + "\\" + dirname
+                Solving(new_path)
+            return filelist
         
 
-my_path ="E:\PROG"
+my_path ="E:\PROG\Temp1"
 filelist1 = FindFiles(my_path)
 if filelist1 == []:
     print("Каталог не найден")
