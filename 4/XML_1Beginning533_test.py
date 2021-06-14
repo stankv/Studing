c362aa2c289381b8a6b2a3c307e5bac713a2dc57
Stan Bad <stankv@mail.ru>
2021-06-14 16:22:00 +0300

    added method setUp to class of tests

--- 4/XML_1Beginning533_test.py
+++ 4/XML_1Beginning533_test.py
@@ -4,29 +4,29 @@ import xml.etree.ElementTree as ETree
 import os.path
 from XML_1Beginning533 import NumberOfNodes
 
-xml1 = ETree.parse(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../4/data/demo.xml'))
-root = xml1.getroot()
 
 class XML1_Test_533(unittest.TestCase):
+
+    def setUp(self):
+        xml1 = ETree.parse(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../4/data/demo.xml'))
+        self.root = xml1.getroot()
+        self.L = []
 
 # РЕГРЕССИОННЫЕ ТЕСТЫ
 # 1. Существующий атрибут
     def test_regression1_1(self):
         attr = "name"
-        L = []
-        self.assertEqual(NumberOfNodes(attr, root, L), 7)
+        self.assertEqual(NumberOfNodes(attr, self.root, self.L), 7)
     
 # 2. Несуществующий атрибут
     def test_regression1_2(self):
         attr = "name777"
-        L = []
-        self.assertEqual(NumberOfNodes(attr, root, L), 0)
+        self.assertEqual(NumberOfNodes(attr, self.root, self.L), 0)
 
 # 3. Пустой атрибут
     def test_regression1_3(self):
         attr = ""
-        L = []
-        self.assertEqual(NumberOfNodes(attr, root, L), 0)
+        self.assertEqual(NumberOfNodes(attr, self.root, self.L), 0)
 
 if __name__ == '__main__':
     unittest.main()
