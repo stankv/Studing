ade411c4857acec8f592ed0a087afa95de9585f4
Stan Bad <stankv@mail.ru>
2021-06-14 14:23:04 +0300

    correctiom code: in function added list as parameter

--- 4/XML_1Beginning532_rec.py
+++ 4/XML_1Beginning532_rec.py
@@ -1,13 +1,13 @@
 # XML-документ: РЕКУРСИВНАЯ функция возвращает список всех значений (по всем узлам) для конкретного тега, если задано его название.
 # Возвращает пустой список, если тег не найден.
+# При вызове функции задавать аргумент List_values = []
 import xml.etree.ElementTree as ETree
 import os.path
 
 xml1 = ETree.parse(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../4/data/demo.xml'))
 root = xml1.getroot()
 
-List_values = []
-def ListValues(tag_name, root):
+def ListValues(tag_name, root, List_values):
     for i in range(len(root)):
         if root[i].tag != tag_name and len(root[i]) == 0:
             continue
@@ -19,5 +19,5 @@ def ListValues(tag_name, root):
                 List_values.append(level2[j].text)
         elif root[i].tag != tag_name and len(root[i]) != 0:
             level2 = root[i]
-            ListValues(tag_name, level2)
+            ListValues(tag_name, level2, List_values)
     return List_values
