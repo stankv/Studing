ef932397c2f6d52e1af449596c7b15da00934027
Stan Bad <stankv@mail.ru>
2021-07-07 12:06:50 +0300

    added Generators1.py

---
+++ 4/Generators1.py
@@ -0,0 +1,40 @@
+# ГЕНЕРАТОРЫ. "Одновременная" работа произвольного числа генераторов.
+# N - число "одновременно" работающих генераторов
+# L - массив, значения которого есть n для функции long_process(id, n)
+import random
+
+def Generators(L):
+    # генератор
+    def long_process(id, n):
+        sum = 0
+        for x in range(n):
+            sum += x
+            print(id, sum)
+            if x < n-1:
+                yield
+            else:
+                yield sum
+
+    # "инициализация" и запуск генераторов
+    N = len(L)    # количество запускаемых генераторов
+    ID = []
+    Id = []
+    R = {}
+    for i in range(N):
+        IDn = "ID" + str(i)
+        ID.append(IDn)
+        IDk = long_process(IDn, L[i])
+        Id.append(IDk)
+        R[IDn] = None
+
+    for j in range(max(L)):
+        for i in range(N):
+            if R[ID[i]] is None: 
+                R[ID[i]] = next(Id[i])
+    print(R)
+
+N0 = random.randint(3, 10)
+L0 = []
+for i in range(N0):
+    L0.append(random.randint(1, 10))    # создаем входные данные - массив случайных целых чисел случайной длины
+Generators(L0)
