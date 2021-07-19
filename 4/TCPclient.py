e1ea0ac254cb1e236ff67dc8014afbe7cffd0674
Stan Bad <stankv@mail.ru>
2021-07-19 15:38:02 +0300

    added infinite loop

--- 4/TCPclient.py
+++ 4/TCPclient.py
@@ -2,6 +2,20 @@
 from socket import socket, AF_INET, SOCK_STREAM
 s = socket(AF_INET, SOCK_STREAM)
 
-s.connect(('localhost', 12345))
-s.send(b'Hello, server')
-print(s.recv(1024).strip())
+s.connect(('localhost', 12345))    # открываем соединение с сервером
+s.send(b'Hello, server')           # отправляем серверу запрос
+server_response = s.recv(1024).decode()    # принимаем ответ сервера
+print(server_response.strip())     # выводим ответ сервера на консоль
+
+flag = True
+while(flag):
+    str1 = input("-->")
+    if str1.strip() == "bye":
+        print("EXIT")
+        flag = False
+    else:
+        s.send(str1.encode())
+        server_response = s.recv(1024).decode()
+        print(server_response.strip())
+
+s.close()    # закрываем соединение с сервером
