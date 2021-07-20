dfa1fb21bd49d7eed951d2124801b6726f1831bf
Stan Bad <stankv@mail.ru>
2021-07-20 11:21:53 +0300

    added simple async server TCPserver_async.py

---
+++ 4/TCPserver_async.py
@@ -0,0 +1,27 @@
+# Простой асинхронный TCP-сервер
+# https://docs-python.ru/standart-library/modul-socketserver-python/klassy-forkingmixin-threadingmixin-modulja-socketserver/
+from socketserver import BaseRequestHandler, TCPServer, ThreadingMixIn
+from threading import Thread
+
+# Класс обработчика клиентских запросов
+class TestTCPHandler(BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address), end='')
+        print(self.data)
+        self.request.sendall(bytes(self.data.upper()))
+
+# Класс асинхронного сервера, который обрабатывает запросы клиентов в отдельных потоках
+class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    pass
+
+#server = ThreadedTCPServer(('localhost', 12345), TestTCPHandler)
+print("Server activated")
+#server.serve_forever()
+with ThreadedTCPServer(('localhost', 12345), TestTCPHandler) as server:
+    # Запускаем поток с сервером - этот поток затем 
+    # запустит еще один поток для каждого запроса
+    server_thread = Thread(target=server.serve_forever())
+    # Выйдем из потока сервера, когда основной поток завершится
+    server_thread.daemon = True
+    server_thread.start()
