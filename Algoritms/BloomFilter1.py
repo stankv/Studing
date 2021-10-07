0c03bdc5ff704e9e4791c062a7ca6c5e2a5747cb
Stan Bad <stankv@mail.ru>
2021-10-07 16:32:59 +0300

    deleted library bitarray, changed class functions

--- Algoritms/BloomFilter1.py
+++ Algoritms/BloomFilter1.py
@@ -1,62 +1,36 @@
-# ФИЛЬТР БЛЮМА НА ОСНОВЕ БИТОВОГО МАССИВА
+# ФИЛЬТР БЛЮМА
 # (входные данные - строки)
-import bitarray
 
 class BloomFilter:
 
     # создаём битовый массив длиной f_len
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.bit_array = bitarray.bitarray()
-        for i in range(self.filter_len):
-            self.bit_array.append(0)
+        self.filter = 0
 
     def hash1(self, str1):
         code = 0
         for i in str1:
             code = ((code * 17) + ord(i)) % self.filter_len
-        return code
+        return 0 | (1 << code)
 
     def hash2(self, str1):
         code = 0
         for i in str1:
             code = ((code * 223) + ord(i)) % self.filter_len
-        return code
+        return 0 | (1 << code)
 
 
     # Метод добавляет строку str1 в фильтр
     def add(self, str1):
-        self.bit_array[self.hash1(str1)] = 1
-        self.bit_array[self.hash2(str1)] = 1
+        self.filter |= self.hash1(str1)
+        self.filter |= self.hash2(str1)
+        return self.filter
+
 
     # Метод проверяет, имеется ли строка str1 в фильтре
     def is_value(self, str1):
-        i1 = self.hash1(str1)
-        i2 = self.hash2(str1)
-        if self.bit_array[i1] == 1 and self.bit_array[i2] == 1:
+        bitmask = self.filter & (self.hash1(str1) | self.hash2(str1))
+        if bitmask == self.hash1(str1) | self.hash2(str1):
             return True
-        else:
-            return False
-
-"""bf = BloomFilter(32)
-bf.add("0123456789")
-print(bf.bit_array)
-bf.add("1234567890")
-print(bf.bit_array)
-bf.add("2345678901")
-print(bf.bit_array)
-bf.add("3456789012")
-print(bf.bit_array)
-bf.add("4567890123")
-print(bf.bit_array)
-bf.add("5678901234")
-print(bf.bit_array)
-bf.add("6789012345")
-print(bf.bit_array)
-bf.add("7890123456")
-print(bf.bit_array)
-bf.add("8901234567")
-print(bf.bit_array)
-bf.add("9012345678")
-print(bf.bit_array)
-print(bf.is_value("5678901234"))"""
+        return False
