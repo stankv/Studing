f5825ce2718e548ce7d8ca67d1f687d9014fded7
Stan Bad <stankv@mail.ru>
2021-12-17 02:30:02 +0300

    added 2_RelationalDatabases.py

---
+++ SQL/2_RelationalDatabases.py
@@ -0,0 +1,13 @@
+# Разберитесь во взаимосвязях между таблицами northwind и опишите их (один-к-одному, один-ко-многим и многие-ко-многим). 
+
+# Categories - Products: один ко многим
+# Contacts: нет связей
+# Customers - CustomerCustomerDemo - CustomerDemographics: многие ко многим
+# Customers - Orders: один ко многим
+# Employees - EmployeeTerritories - Territories: многие ко многим
+# Employees - Orders: один ко многим
+# Orders - Order Details - Products: многие ко многим
+# Orders - Shippers: один ко многим
+# Customers - Orders: один ко многим
+# Products - Suppliers: один ко многим
+# Region - Territories: один ко многим
