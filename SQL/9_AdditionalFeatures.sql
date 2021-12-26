3bd6d335208bfabddb98c5f6e54ba2d9dbdd967b
Stan Bad <stankv@mail.ru>
2021-12-26 22:49:03 +0300

    fixed errors

--- SQL/9_AdditionalFeatures.sql
+++ SQL/9_AdditionalFeatures.sql
@@ -1,10 +1,10 @@
 -- 9.4. Задания
 
 -- 9.4.1. Найдите все пары из разных заказчиков (Customers), для которых не задан регион (поле Region).
-SELECT t1.ContactName, t2.ContactName, t2.Region
+SELECT t1.ContactName, t2.ContactName, t1.Region
 FROM Customers t1, Customers t2 
-WHERE (t1.Region IS NULL) AND (t2.Region IS NULL)
-ORDER BY t1.ContactName;
+WHERE (t1.Region IS NULL) AND (t2.Region IS NULL) AND
+      (t1.CustomerID <> t2.CustomerID)
 
 -- 9.4.2. Найдите вложенным запросом список заказов (Orders), в котором у заказчиков (Customers) регион не пуст (поле Region).
 SELECT * FROM Orders t1 
@@ -12,8 +12,19 @@ WHERE EXISTS
 (SELECT * FROM Customers t2 
 WHERE t1.CustomerID = t2.CustomerID AND t2.Region IS NOT NULL);
 
+-- или:
+SELECT OrderID
+FROM Orders t1
+WHERE CustomerID = ANY 
+  (SELECT CustomerID FROM Customers
+   WHERE Region IS NOT NULL);
+
 /* 9.4.3. Немного условный, но показательный пример. Найдите все заказы (таблица Orders), цена за доставку товара которых 
 (Freight) превышает цену любого товара (поле UnitPrice, таблица Products). */
-ELECT * FROM Orders
+SELECT * FROM Orders
 WHERE Freight > ANY
 (SELECT MAX(UnitPrice) FROM Products);
+-- это не правильно. правильно вот так:
+SELECT * FROM Orders
+WHERE Freight > ALL
+(SELECT UnitPrice FROM Products);
