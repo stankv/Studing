52b3102a3b7728d947b553ae60232c76a8f6100c
Stan Bad <stankv@mail.ru>
2021-12-30 15:48:41 +0300

    fixed errors

--- SQL/11_JoinAndUnion.sql
+++ SQL/11_JoinAndUnion.sql
@@ -1,15 +1,13 @@
 -- 11.5. Задания
 
 -- 11.5.1. Отберите с помощью LEFT JOIN все записи из таблицы Customers, для которых FK-ключ таблицы Orders пустой.
-SELECT Customers.*, Orders.CustomerID FROM Customers 
-LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID AND Orders.CustomerID IS NULL
-ORDER BY Customers.CompanyName; 
+SELECT * FROM Customers LEFT JOIN Orders
+ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.CustomerID IS NULL
 
 /* 11.5.2. Выведите конкретную информацию по всем пользователям Customers и поставщикам Suppliers - имя контактной 
 персоны, город и страну, а также идентификацию группы (пользователь или поставщик). */
 SELECT 'Customer' As Type, ContactName, City, Country FROM Customers 
-WHERE Country='USA' 
 UNION 
 SELECT 'Supplier' As Type, ContactName, City, Country FROM Suppliers 
-WHERE Country='USA' 
-ORDER BY City; 
+ORDER BY Type
